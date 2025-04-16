c74f159ef684a7a76340c4624cbf29fcf9675ecf
MarinaDA99 <adelinambudau@gmail.com>
2025-04-16 20:45:30 +0200

    Update NutriMind_app_completa.py

--- NutriMind_app_completa.py
+++ NutriMind_app_completa.py
@@ -132,7 +132,6 @@ if os.path.exists(archivo_csv):
         st.markdown(f"🌿 Esta semana has consumido **{len(grupos_vegetales)} / 30** vegetales distintos.")
             else:
                 st.info("Aún no hay datos registrados esta semana.")
-            else:
-                st.info("Aún no has guardado ningún registro.")
+         
 
 
